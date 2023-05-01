b22da80454892cfad89e5e0a40f2eaf066cbc16c
juvin <juvin1234@gmail.com>
2023-05-02 01:01:00 +0700

    enhance Rating

--- app/src/main/java/com/nukang/app/rating/RatingController.java
+++ app/src/main/java/com/nukang/app/rating/RatingController.java
@@ -1,5 +1,8 @@
 package com.nukang.app.rating;
 
+import com.nukang.app.user.AppUser;
+import com.nukang.app.user.AppUserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -8,17 +11,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/v1/rating")
 public class RatingController {
-    @Autowired
-    RatingService ratingService;
+    private final RatingService ratingService;
+    private final AppUserRepository appUserRepository;
 
     @PostMapping("")
-    public ResponseEntity postRating(@RequestBody Rating rating) {
+    public ResponseEntity postRating(@RequestBody Rating rating, Principal principal) {
         Rating posted = null;
+        AppUser appUser = appUserRepository.findByUsername(principal.getName()).orElse(null);
         try {
-            posted = ratingService.post(rating);
+            posted = ratingService.post(rating,appUser);
             return ResponseEntity.ok(posted);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
--- app/src/main/java/com/nukang/app/rating/RatingService.java
+++ app/src/main/java/com/nukang/app/rating/RatingService.java
@@ -1,18 +1,22 @@
 package com.nukang.app.rating;
 
+import com.nukang.app.user.AppUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
 public class RatingService {
     @Autowired
     RatingRepository ratingRepository;
-    public Rating post(Rating rating) throws Exception {
+    public Rating post(Rating rating, AppUser appUser) throws Exception {
         String ratingId = UUID.randomUUID().toString();
         validateRating(rating);
         rating.setId(ratingId.replace("-",""));
+        rating.setCreateDate(LocalDateTime.now());
+        rating.setCreatedBy(appUser.getUserId());
         ratingRepository.save(rating);
         return rating;
     }
