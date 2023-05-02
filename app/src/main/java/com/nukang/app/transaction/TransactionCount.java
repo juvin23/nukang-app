e5aeee04732ee835bb35e15f26a621a716a5c71d
juvin <juvin1234@gmail.com>
2023-05-03 06:07:48 +0700

    count transaction bug

--- app/src/main/java/com/nukang/app/transaction/TransactionCount.java
+++ app/src/main/java/com/nukang/app/transaction/TransactionCount.java
@@ -1,6 +1,11 @@
 package com.nukang.app.transaction;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class TransactionCount {
     int count;
-    int total;
+    int amount;
 }
